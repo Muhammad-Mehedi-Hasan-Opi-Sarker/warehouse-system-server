5774d75641d39a957a9203d5b99525a1b5471739
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-28 17:36:18 +0600

    add single product

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const express = require('express')
 const app = express()
 let cors = require('cors');
-const port = 5000
+const port = process.env.PORT || 5000;
 require('dotenv').config()
-
-// user name = warehouse
-// password= rdsqSCESSge7jeOO
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mory7js.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    
+
     try {
         const database = client.db("database").collection("productCollection");
+
         app.get('/product', async (req, res) => {
             const query = {};
-            const cursor = database.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
+            const cursor = database.find(query)
+            const product = await cursor.toArray();
+            res.send(product)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await database.findOne(query);
+            res.send(result);
+        })
+
     }
 
     finally {
