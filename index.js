b5cf1e73f42192ab3edac8e6fdc57e9a968addf0
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-29 12:49:31 +0600

    dlete item api

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/product/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await database.deleteOne(query);
+            res.send(result);
+        })
+
     }
 
     finally {
